22cfe7703a6ceeb647903ea3f5880d28b5338820
Crtmetton <crtmetton@gmail.com>
2023-06-26 12:00:02 +0200

    Add Php

---
+++ Index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Asset/Css/stylesheet.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="cureent-ver-main">
+      <?php include 'Top-Bar.php'?>
+        <div class="Mid">
+          <div class="group-3">
+            <div class="ellipse-1"></div>
+            <img id="Logo" src="Asset/Img/Logo-BOTGES-NoBack.png"/>
+          </div>
+          <div class="Tool-for-stud">
+            Un outil pour aider les etudiants
+          </div>
+          <div class="add-to-servv">
+            <div class="group-6">
+              <div class="support4"></div>
+              <button class="BTN-Type" type="">Ajoute le a ton serveur ?</button>
+            </div>
+          </div>
+        </div>
+      </div>
+</body>
+</html>
